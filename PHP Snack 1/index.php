039a412e0dca7115adc7e3946393212162620b57
Federico <federico.scarpati1989@gmail.com>
2021-01-29 15:52:34 +0100

    Primo snack

--- PHP Snack 1/index.php
+++ PHP Snack 1/index.php
@@ -4,6 +4,22 @@ Ogni array della partita avrà una squadra di casa e una squadra ospite, punti
 fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55 - 60 -->
+<?php
+    $matches = [
+        [
+            "homeTeam" => "Olimpia Milano",
+            "awayTeam" => "Cantù",
+            "homePoints" => "55",
+            "awayPoints" => "60",
+        ],
+        [
+            "homeTeam" => "Virtus Bologna",
+            "awayTeam" => "Brescia",
+            "homePoints" => "89",
+            "awayPoints" => "90",
+        ],
+    ];
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +29,10 @@ Olimpia Milano - Cantù | 55 - 60 -->
         <title>PHP Snack 1</title>
     </head>
     <body>
-        
+    <h1><?php for($i=0; $i < count($matches); $i++){
+        echo $matches[$i]["homeTeam"] . " - " . $matches[$i]["awayTeam"] . " / " . $matches[$i]["homePoints"] . " - " . $matches[$i]["awayPoints"] ; 
+        ?> <br> <?php
+    } ?>
+    </h1>
     </body>
 </html>
