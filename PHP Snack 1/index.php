c240e1865729a2c49ee5e727090485c60087329e
Federico <federico.scarpati1989@gmail.com>
2021-01-29 14:55:44 +0100

    Primo commit
    Inserimento dei file

---
+++ PHP Snack 1/index.php
@@ -0,0 +1,18 @@
+<!-- Creiamo un array 'matches' contenente altri array i quali 
+rappresentano delle partite di basket di un’ipotetica tappa del calendario. 
+Ogni array della partita avrà una squadra di casa e una squadra ospite, punti 
+fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55 - 60 -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Snack 1</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
